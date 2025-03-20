68116eda35c4989b4dc52f79f9935439695c5c5d
seongyeon-lee-fitfuns <sy.lee@fitfuns.com>
2025-03-20 14:41:37 +0900

    wip: lua test

--- server/data/modules/main.lua
+++ server/data/modules/main.lua
@@ -1,10 +1,19 @@
 local nk = require("nakama")
 
-nk.logger.info("Hello, world!")
+nk.logger_info("Hello, world!")
 
-nk.run_once(function()
-    nk.logger.info("THIS SHOULD ONLY RUN ONCE")
-end)
+nk.register_rpc(function(context, payload)
+    return "Hello, world! This is the my first lua rpc"
+end, "hello")
 
+nk.register_req_before(function(context, payload)
+    nk.logger_info("Before AddFriends")
+    return payload
+end, "AddFriends")
+
+nk.register_req_after(function(context, payload)
+    nk.logger_info("After AddFriends")
+    return payload
+end, "AddFriends")
 
 
